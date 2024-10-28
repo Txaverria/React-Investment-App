6f4614fd2812cca78ba0e3d497499523497940af
Isaac Chavarría <89037579+Txaverria@users.noreply.github.com>
2024-10-28 06:56:04 -0600

    Update ResultsTable.jsx

--- src/components/ResultsTable.jsx
+++ src/components/ResultsTable.jsx
@@ -1,3 +1,24 @@
 export default function ResultsTable() {
-  return <></>;
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>16800</td>
+          <td>900</td>
+          <td>900</td>
+          <td>15900</td>
+        </tr>
+      </tbody>
+    </table>
+  );
 }
