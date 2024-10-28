30c5e398701a5f599fd558f0bbaf9422e1daa1ec
Isaac Chavarría <89037579+Txaverria@users.noreply.github.com>
2024-10-28 06:40:23 -0600

    Add UserInput functonality

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,10 @@
+export default function UserInput() {
+  return (
+    <div id="user-input">
+      <div className="input-group">
+        <label htmlFor="initial-investment">Initial Investment</label>
+        <input type="number" />
+      </div>
+    </div>
+  );
+}
