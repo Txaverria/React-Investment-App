ba37d6b720be190918bc2f542aa5190d0c1ce5f7
Isaac Chavarría <89037579+Txaverria@users.noreply.github.com>
2024-10-29 06:58:58 -0600

    Set UserInput functionality for lifting state up

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,26 +1,49 @@
-export default function UserInput() {
   return (
-    <div id="user-input">
+    <section id="user-input">
       <div className="input-group">
         <p>
           <label htmlFor="initial-investment">Initial Investment</label>
-          <input type="number" required />
+          <input
+            type="number"
+            name="initialInvestment"
+            value={fieldsData.initialInvestment}
+            onChange={handleChange}
+            required
+          />
         </p>
         <p>
           <label htmlFor="annual-investment">Annual Investment</label>
-          <input type="number" required />
+          <input
+            type="number"
+            name="annualInvestment"
+            value={fieldsData.annualInvestment}
+            onChange={handleChange}
+            required
+          />
         </p>
       </div>
       <div className="input-group">
         <p>
           <label htmlFor="return">Expected Return</label>
-          <input type="number" required />
+          <input
+            type="number"
+            name="expectedReturn"
+            value={fieldsData.expectedReturn}
+            onChange={handleChange}
+            required
+          />
         </p>
         <p>
           <label htmlFor="duration">Duration</label>
-          <input type="number" required />
+          <input
+            type="number"
+            name="duration"
+            value={fieldsData.duration}
+            onChange={handleChange}
+            required
+          />
         </p>
       </div>
-    </div>
+    </section>
   );
 }
