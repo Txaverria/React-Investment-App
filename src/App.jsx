1896b352c36d4a657e5e6fdd730e336eda5428ba
Isaac Chavarría <89037579+Txaverria@users.noreply.github.com>
2024-10-29 06:59:48 -0600

    Update App for useState with lifted state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
+
 import Header from "./components/Header";
 import UserInput from "./components/UserInput";
 import ResultsTable from "./components/ResultsTable";
 
 function App() {
+  const [inputData, setInputData] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  // Function to handle changes from UserInput
+  const handleInputChange = (newInputData) => {
+    setInputData(newInputData); // Update the shared state
+  };
+
   return (
     <>
-      <Header></Header>
-      <UserInput></UserInput>
-      <ResultsTable></ResultsTable>
+      <Header />
+      <UserInput fieldsData={inputData} onInputChange={handleInputChange} />
+      <ResultsTable data={inputData} />
     </>
   );
 }
